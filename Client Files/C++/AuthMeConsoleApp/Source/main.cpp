de0ee9a9b9f3a4f13214d19a2d5465ef09b5121a
Noodle <christianstechhelp@gmail.com>
2023-07-21 18:05:17 -0400

    Added Example Console App to showcase how to use AuthMeLib.

---
+++ Client Files/C++/AuthMeConsoleApp/Source/main.cpp
@@ -0,0 +1,9 @@
+// main.cpp : This file contains the 'main' function. Program execution begins and ends there.
+
+#include "main.hpp"
+
+// main : Start of the program.
+int main()
+{
+    
+}
---
+++ Client Files/C++/AuthMeConsoleApp/Source/main.hpp
@@ -0,0 +1,10 @@
+// main.hpp : This file declares the functions that will used in main.cpp
+
+#include <Windows.h>
+#include <iostream>
+
+// Console : This namespace will hold all example functions and variables used.
+namespace Console
+{
+
+}

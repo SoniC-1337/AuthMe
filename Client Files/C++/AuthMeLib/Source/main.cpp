4abb2dec04bdcd2189b2ff3f58a834224a6e75fd
Noodle <christianstechhelp@gmail.com>
2023-07-21 17:37:03 -0400

    Added C++ Folder and AuthMeLib Project.

---
+++ Client Files/C++/AuthMeLib/Source/main.cpp
@@ -0,0 +1,24 @@
+// main.cpp : Defines the functions for the static library.
+
+#include "main.h"
+
+// Login: Authenticate with the server in order to successfully login user.
+int AuthMe::Login()
+{
+	// TODO: Add login logic.
+	return 0;
+}
+
+// Register: Authenticate with the server in order to successfully register user.
+int AuthMe::Register()
+{
+	// TODO: Add register logic.
+	return 0;
+}
+
+// Download: Authenticate with the server in order to successfully download data for the user.
+int AuthMe::Download()
+{
+	// TODO: Add download logic.
+	return 0;
+}
---
+++ Client Files/C++/AuthMeLib/Source/main.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "../Headers/pch.h"
+#include "../Headers/framework.h"
+
+class AuthMe
+{
+private:
+
+public:
+	int Login();
+	int Register();
+	int Download();
+};

7d29fe3b25fb911e6ebe3584fdf97c4912d776a2
Noodle <christianstechhelp@gmail.com>
2023-07-21 18:20:16 -0400

    Added some helper functions in Console namespace.

--- Client Files/C++/AuthMeConsoleApp/Source/main.hpp
+++ Client Files/C++/AuthMeConsoleApp/Source/main.hpp
@@ -6,5 +6,27 @@
 // Console : This namespace will hold all example functions and variables used.
 namespace Console
 {
+	// Cout : Shorthand std::cout into a function for quicker coding.
+	void Cout(const char* text, const char* endl)
+	{
+		std::cout << text << endl;
+	}
 
+	// Printf : Shorthand printf() into a function for quicker coding.
+	void Printf(const char* text, auto format)
+	{
+		printf(text, format);
+	}
+
+	// Cls : Shorthand system("cls") into a function for quicker coding.
+	void Cls()
+	{
+		system("cls");
+	}
+
+	// Pause : Shorthand system("pause") into a function for quicker coding.
+	void Pause()
+	{
+		system("pause");
+	}
 }
